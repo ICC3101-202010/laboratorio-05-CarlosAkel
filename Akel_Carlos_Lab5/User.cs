2bb5437d875e9b27e56e5806e8792f0b755eec82
CarlosAkel <cmakel@miuandes.cl>
2020-04-22 23:11:22 -0400

    Entrada de codigo u

---
+++ Akel_Carlos_Lab5/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solucion_Lab_21_abril
+{
+    class User
+    {
+        public delegate void EmailCheckEventHandler(object source, EventArgs args);
+
+        public event EmailCheckEventHandler EmailVerified;
+
+        protected virtual void OnEmailVeridied()
+        {
+            if(EmailVerified != null)
+            {
+                EmailVerified(this, EventArgs.Empty);
+            }
+        }
+
+        public void OnEmailSent(object source, EventArgs args)
+        {
+            Console.WriteLine("Do you want to verify your email? (yes/no)");
+            string answ = Console.ReadLine();
+            if (answ == "yes")
+            {
+                OnEmailVeridied();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
